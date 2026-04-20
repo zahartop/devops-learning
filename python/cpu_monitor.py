293aa05904551a2ddd313df40c2f1d2085e44c79
zahartop <skskxnddndnx@inbox.ru>
2026-04-20 22:21:52 +0300

    Delete python directory

--- python/cpu_monitor.py
+++
@@ -1,5 +0,0 @@
-
-python
-import psutil
-print (f"CPU usage: {psutil.cpu_perecent()}%")
-
--- python/disk_usage.py
+++
@@ -1,8 +0,0 @@
-python
-import shutil
-total, used, free =shutill.disk_usage("/")
-print (f"Total: {total // (2**30)} GB")
-print (f"Used: {used // (2**30)} GB")
-print (f"Free: {free // (2**30)}  GB")
-
-
